c7fd1421ccd856c02faa986465b6383298b1c2fb
Younes ASSASLA <assaslayounes@yahoo.fr>
2026-06-03 01:29:42 +0100

    change url backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
-
+//"http://localhost:5000/"
+//https://injaz-backend.onrender.com
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL ?? "http://localhost:5000/",
+  baseURL: import.meta.env.VITE_API_BASE_URL ?? "https://injaz-backend.onrender.com/",
   headers: { "Content-Type": "application/json" },
   timeout: 12000
 });
