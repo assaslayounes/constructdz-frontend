3cc2aa3a51ac209e694a72b55d21edc2fc513b67
Younes ASSASLA <assaslayounes@yahoo.fr>
2026-06-08 10:58:11 +0100

    add Applayout

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -2,9 +2,10 @@ import { Outlet } from "react-router-dom";
 import { Footer } from "@/components/layout/Footer";
 import { Navbar } from "@/components/layout/Navbar";
 
+
 export function AppLayout() {
   return (
-    <main className="app-shell relative min-h-screen pb-[60px] pt-16 lg:pb-0 lg:pt-[72px]">
+     <main className="app-shell relative min-h-screen pb-[60px] pt-16 lg:pb-0 lg:pt-[72px]">
       <Navbar />
       <Outlet />
       <Footer />
